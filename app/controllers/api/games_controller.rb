40a8c59a05f87d615dd5519689337d19e9a43e67
Mat <mvello66@gmail.com>
2021-04-15 20:44:21 -0400

    useful comments in games controller so i know what im actually reading days from now heh

--- app/controllers/api/games_controller.rb
+++ app/controllers/api/games_controller.rb
@@ -1,28 +1,28 @@
 class Api::GamesController < ApplicationController
   def create
+    # generates new game
     @game = Game.new({})
     @game.save
-
+    #generates new user
     @user = User.new({
       name: params[:user_name],
-      game_id: @game.id,
+      game_id: @game.id, #assigns user to current game
       base_health: 50,
     })
     @user.save
-
+    #generates new room
     @room = Room.new({
       name: "Dungeon" + "" + Random.rand(1..20).to_s,
-      game_id: @game.id,
+      game_id: @game.id, #assigns room to current game
 
     })
     @room.save!
 
     @game.update({
-      current_room: @room.id,
+      current_room: @room.id, #assigns current game room to newly created
     })
     @game.save
 
-    #/ needs to render actual game instead of information
     render "game_info.json.jb"
   end
 
@@ -34,13 +34,14 @@ class Api::GamesController < ApplicationController
 
   def update
     @game = Game.find_by(id: params[:id])
+    #creates new room
     @room = Room.new({
       name: "Dungeon" + "" + Random.rand(1..20).to_s,
       game_id: @game.id,
 
     })
     @room.save!
-
+    #then updates current game room
     @game.update({
       current_room: @room.id,
     })
